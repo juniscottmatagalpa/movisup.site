6edc1c44f09808101dc8ae1a0b360ac763f92ef3
juniscottmatagalpa <juniscottmatagalpa@gmail.com>
2026-06-18 09:52:32 -0600

    Update partido.php

--- partido.php
+++ partido.php
@@ -1,19 +1,22 @@
 <?php
 
-header('Content-Type: application/json');
+header('Content-Type: application/json; charset=utf-8');
 
-$url='https://raw.githubusercontent.com/openfootball/worldcup.json/master/2026--world-cup/matches.json';
 
-$data=@file_get_contents($url);
+$url = 'https://raw.githubusercontent.com/openfootball/world-cup.json/master/2026--world-cup/matches.json';
+
+
+$data = @file_get_contents($url);
 
 
 if(!$data){
 
 echo json_encode([
 
-"titulo"=>"Mundial FIFA 2026 EN VIVO",
+'partido' => 'Mundial FIFA 2026 EN VIVO',
 
-"descripcion"=>"Transmisión en vivo"
+'descripcion' =>
+'Aquí podrás disfrutar de las transmisiones y seguir todos los partidos de la Copa Mundial FIFA 2026 en vivo y gratis.'
 
 ]);
 
@@ -22,40 +25,78 @@ exit;
 }
 
 
-$json=json_decode($data,true);
+$json = json_decode($data, true);
 
-$ahora=time();
 
-$titulo="Mundial FIFA 2026 EN VIVO";
 
-$descripcion="Transmisión en vivo";
+$ahora = time();
 
+
+$partido = 'Mundial FIFA 2026 EN VIVO';
+
+
+$descripcion =
+'Aquí podrás disfrutar de las transmisiones y seguir todos los partidos de la Copa Mundial FIFA 2026 en vivo y gratis.';
+
+
+
+if(isset($json['matches'])){
 
 
 foreach($json['matches'] as $p){
 
 
-$inicio=strtotime($p['date']);
+if(!isset($p['date']))
+continue;
 
-$fin=$inicio+(2*60*60);
+
+
+$inicio = strtotime($p['date']);
+
+
+
+/*
+Duración estimada:
+
+120 minutos
+
+*/
+
+
+$fin = $inicio + (2 * 60 * 60);
 
 
 
 if($ahora >= $inicio && $ahora <= $fin){
 
-$titulo=
 
-$p['team1']['name'].
-' vs '.
-$p['team2']['name'].
+$equipo1 = $p['team1']['name'] ?? '';
+
+$equipo2 = $p['team2']['name'] ?? '';
+
+
+
+$partido =
+
+$equipo1 .
+' vs ' .
+$equipo2 .
 ' EN VIVO';
 
 
 
-$descripcion=
+$descripcion =
 
-$titulo.
-' | Mundial FIFA 2026';
+'Aquí podrás ver el partido de '.
+
+$equipo1.
+
+' vs '.
+
+$equipo2.
+
+' EN VIVO correspondiente al Mundial FIFA 2026 completamente en vivo y gratis.';
+
 
 
 break;
@@ -67,13 +108,20 @@ break;
 }
 
 
+}
+
+
+
 
 echo json_encode([
 
-"titulo"=>$titulo,
+'partido'=>$partido,
 
-"descripcion"=>$descripcion
+'descripcion'=>$descripcion
 
-]);
+
+],JSON_UNESCAPED_UNICODE);
+
+
 
 ?>
